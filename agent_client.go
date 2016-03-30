5be566bb80988934c7abb3c74b1096bd79f8958e
绯浅yousa <yousa@feiqianyousadeMacBook-Pro.local>
2016-03-30 14:32:22 +0800

    ./agent_client.go IP

--- agent_client.go
+++ agent_client.go
@@ -5,9 +5,25 @@ import (
 	
 	"fmt"
 	"os"
+	"net"
 )
 
 func main() {
+	
+	if len(os.Args) != 2{
+		fmt.Fprintf(os.Stderr, "Usage: %s ip-addr\n", os.Args[0])
+		os.Exit(0)
+	}	
+	
+	IPstr := os.Args[1]
+	addr := net.ParseIP(IPstr)
+	if addr == nil{
+		fmt.Println("Invalid address")	
+		os.Exit(0)
+	} else {
+		fmt.Println("Connect Address is", addr)	
+	}
+	
 	fmt.Println("Connect to server")
 	
 	requester, err := zmq.NewSocket(zmq.REQ)
@@ -16,7 +32,11 @@ func main() {
 		os.Exit(1)	
 	}
 	
-	requester.Connect("tcp://localhost:5555")
+	peer_addr := "tcp://" + addr.String() + ":5555"
+	fmt.Println("peer_addr is", peer_addr)
+	
+	requester.Connect(peer_addr)
+	//requester.Connect("tcp://localhost:5555")
 	
 	for i := 1; i < 10; i++ {
 		msg := fmt.Sprintf("Hello %d", i)
